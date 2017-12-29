19a78de9418acc3cea5724d010833273a1a2fa84
xwkuang5 <louisconfussing@gmail.com>
2017-12-29 17:29:21 -0500

    Add explanations

--- clrs/kmp.cpp
+++ clrs/kmp.cpp
@@ -25,6 +25,35 @@ std::vector<int> longest_prefix_suffix(const std::string& pattern) {
             } else {
                 // previous length != 0 and current character does not match
                 // decrease length (do not update i) and see if there's a match
+
+                /*
+                 *  Suppose we are trying to compute the longest prefix suffix array (lps) for the following pattern:
+                 *  \begin{equation*}
+                 *      a_0, a_1, \cdots, a_{-1}, a^\star
+                 *  \end{equation*}
+                 *  Let's further suppose that $\text{lps}[-2] = m$, i.e., $"a_0, \cdots, a_{m-1}"$ is the same as $"a_{-m}, \cdots, a_{-1}"$ and $a_m != a^\star$. Now, because $a_m != a^\star$, we should decrease the length that we are trying to match, in particular, according to the algorithm we set $\text{len} = \text{lps}[m-1]$ \footnote{Note that we can not simply update len by $\text{len} = \text{len} - 1$ because the resulting match position is not the same. Consider the pattern "acaacc", using the update rule just mentioned will result in incorrect value for the last position.}. Note that we know the following:
+                 *  \begin{align*}
+                 *          a_0 & = a_{-m}\\
+                 *          a_1 & = a_{-(m-1)}\\
+                 *          & \cdots\\
+                 *          a_{m-1} & = a_{-1}
+                 *  \end{align*}
+                 *  Now suppose that $\text{lps}[m-1] = l$, we know the following:
+                 *  \begin{align*}
+                 *          a_0 & = a_{m-l}\\
+                 *          a_1 & = a_{m-l+1}\\
+                 *          & \cdots\\
+                 *          a_{l-1} & = a_{m-1}
+                 *  \end{align*}
+                 *  Therefore, we have the following:
+                 *  \begin{align*}
+                 *          a_{0} & = a_{-l}\\
+                 *          a_{l-2} & = a_{-2}\\
+                 *          & \cdots\\
+                 *          a_{l-1} & = a_{-1}
+                 *  \end{align*}
+                 *  Because of the above, we can safely continue matching from $\text{pat}[l] == a^\star$
+                 */
                 len = lps[len-1];
             }
         }
@@ -34,7 +63,7 @@ std::vector<int> longest_prefix_suffix(const std::string& pattern) {
 }
 
 int main() {
-    std::string test_pattern = "abab";
+    std::string test_pattern = "aaabaaa";
     std::vector<int> result = longest_prefix_suffix(test_pattern);
     for (std::vector<int>::iterator it = result.begin(); it != result.end(); it++) {
         std::cout << *it << " ";
