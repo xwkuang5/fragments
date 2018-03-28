1d2c616df6061fbd02fe172c316b89cbc00eceb2
xwkuang5 <louisconfussing@gmail.com>
2018-03-28 09:36:43 -0400

    Add simple permutation implementation

--- haskell/ProgrammingInHaskell.hs
+++ haskell/ProgrammingInHaskell.hs
@@ -16,3 +16,19 @@ safetail'   xs
 safetail''  []          = []
 safetail''  (_:xs)      = xs
 
+-- 9.11.1
+-- Use the idea of swapping, but instead of swapping, we use inserting
+-- Is there a more efficient way of doing this in Haskell?
+insertAt :: (Num t1, Eq t1) => t -> t1 -> [t] -> [t]
+insertAt e _ []     = [e]
+insertAt e 0 xs     = e:xs
+insertAt e n (x:xs) = x:(insertAt e (n-1) xs)
+
+permsHelper :: (Num n, Enum n, Eq n) => [a] -> n -> [[a]]
+permsHelper [x] 1    = [[x]]
+permsHelper (x:xs) n = [insertAt x l permuted | l <- [0..(n-1)],
+                                                permuted <- permsHelper xs (n-1)]
+                                    
+perms :: [a] -> [[a]]
+perms [] = [[]]
+perms xs = permsHelper xs $ length xs
